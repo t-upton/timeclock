cd3c1c3c00a4bb870f989b9ab14b0f17c1a9fbdd
Tyler Upton <110835525+t-upton@users.noreply.github.com>
2023-08-07 19:40:21 -0500

    Update calculate_time.py
    rewrote so it will print all users in date range with total hours worked.

--- calculate_time.py
+++ calculate_time.py
@@ -1,22 +1,23 @@
 import pandas as pd
 
-def get_total_duration(name, start_date, end_date):
-    # Read the CSV data from a file named "times.csv"
-    df = pd.read_csv("times.csv", parse_dates=[3])  # Parsing the timestamp as date
-    
-    # Filter data
-    mask = (df['Name'] == name) & (df['Timestamp'] >= start_date) & (df['Timestamp'] <= end_date)
-    filtered_data = df[mask]
-    
-    # Calculate total duration
-    total_duration = filtered_data['Duration'].sum()
+def get_total_durations(start_date, end_date):
+    # Define column names and read CSV
+    columns = ["Name", "PIN", "Action", "Timestamp", "Duration"]
+    df = pd.read_csv("times.csv", names=columns, parse_dates=["Timestamp"])
 
-    return total_duration
+    # Filter data based on date range and action type
+    mask = (df['Timestamp'] >= start_date) & (df['Timestamp'] <= end_date) & (df['Action'] == 'clock-out')
+    filtered_data = df[mask]
+
+    # Group by 'Name' and sum 'Duration'
+    total_durations = filtered_data.groupby('Name')['Duration'].sum()
+
+    return total_durations
 
 if __name__ == "__main__":
-    name = input("Enter the name: ")
     start_date = input("Enter the beginning date (YYYY-MM-DD HH:MM:SS): ")
     end_date = input("Enter the end date (YYYY-MM-DD HH:MM:SS): ")
-    
-    total = get_total_duration(name, start_date, end_date)
-    print(f"Total duration for {name} between {start_date} and {end_date}: {total:.2f} hours")
+
+    total_durations = get_total_durations(start_date, end_date)
+    for name, total in total_durations.items():  # Changed from iteritems() to items()
+        print(f"Total duration for {name} between {start_date} and {end_date}: {total:.2f} hours")
